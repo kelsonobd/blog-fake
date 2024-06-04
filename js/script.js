f85d27c8f6fc1718d316c5036547bb7bdb50dfa8
Kelsonobd <kelsonobd@gmail.com>
2024-06-04 10:45:27 -0300

    parte1-finalizada

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-//https://jsonplaceholder.typpicode.com/posts
+//https://jsonplaceholder.typicode.com/posts
 
 async function readPosts() {
     let postArea = document.querySelector(`.posts`);
     postArea.innerHTML = 'Carregando...'
 
-    let response = await fetch('https://jsonplaceholder.typpicode.com/posts')
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
     let json = await response.json();
+
+    if (json.length > 0) {
+        postArea.innerHTML='';
+
+        for(let i in json) {
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`
+            postArea.innerHTML += postHtml;
+        }
+
+
+    } else {
+        postArea.innerHTML = 'Nennhum post para exibir'
+    }
 
 }
 
